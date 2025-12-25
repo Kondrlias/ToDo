478c4d7aa47caf087b03da961af4bb38f6213fba
Julia Kondratsiuk <kondrlias@gmail.com>
2025-12-25 23:51:59 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import './App.css'
 import NoContent from './components/NoContent'
 import LogIn from './components/User/LogIn'
@@ -21,15 +21,15 @@ function App() {
 
   return (
     <>
-
-      <Routes>
-        <Route path='/' element={<LogIn email={email} password={password} token={token} />} />
-        <Route path='/registration' element={<Registration email={email} password={password} username={username} age={age} gender={gender} />} />
-        <Route path='/usertasks' element={token ? <UserTasks token={token} /> : <LogIn />} />
-        <Route path="*" element={<NoContent />} />
-        <Route path="/thanks" element={<Thanks />} />
-      </Routes>
-
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<LogIn email={email} password={password} token={token} />} />
+          <Route path='/registration' element={<Registration email={email} password={password} username={username} age={age} gender={gender} />} />
+          <Route path='/usertasks' element={token ? <UserTasks token={token} /> : <LogIn />} />
+          <Route path="*" element={<NoContent />} />
+          <Route path="/thanks" element={<Thanks />} />
+        </Routes>
+      </HashRouter>
     </>
 
   )
