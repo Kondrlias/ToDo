eb15ee5ac902317e02e6ff314dc25b21fc421586
Julia Kondratsiuk <kondrlias@gmail.com>
2026-01-21 17:31:20 +0300

    fix: fix storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import './App.css'
 import Header from './components/Header'
 import InputTask from './components/InputTask'
 import ShowTasks from './components/ShowTasks'
+useEffect
 
 function App() {
+  const { tasks } = useSelector((store) => store.tasks)
 
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks])
   return (
     <>
       <Header />
