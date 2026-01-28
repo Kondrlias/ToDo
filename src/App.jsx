853b7bcc93e0dd87f2172e32afb8bb9746190345
Julia Kondratsiuk <kondrlias@gmail.com>
2026-01-28 11:30:26 +0300

    fix: fix app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import './App.css';
-import Header from './components/Header';
-import InputTask from './components/InputTask';
-import ShowTasks from './components/ShowTasks';
-import { selectTasks } from './redux/slices/tasksSlice';
+import './App.css'
+import Header from './components/Header'
+import InputTask from './components/InputTask'
+import ShowTasks from './components/ShowTasks'
+
 
 function App() {
-  const tasks = useSelector(selectTasks);
 
-  useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }, [tasks]);
   return (
     <>
       <Header />
       <InputTask />
       <ShowTasks />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
